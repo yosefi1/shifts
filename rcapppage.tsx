6415f1b65493a7a27168dc7529270d1aaa9681ce
yosefi1 <sdoko2@gmail.com>
2025-08-31 22:54:13 +0300

    Fix page.tsx with custom shifts app

---
+++ rcapppage.tsx
@@ -0,0 +1,4 @@
+[33md247de6[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m Fix missing dependencies for Vercel deployment
+[33m6a728cd[m Initial commit: Complete shifts app setup
+[33m9cd7eb5[m Initial commit: Next.js shifts app setup
+[33m118a024[m Initial commit from Create Next App
