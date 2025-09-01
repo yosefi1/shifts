9adb30a880a9b9af6b7b0530bb22d9fd8e305ef3
yosefi1 <sdoko2@gmail.com>
2025-09-01 10:25:48 +0300

    Render ShiftsApp on home; add minimal ShiftsApp component

---
+++ src/components/ShiftsApp.tsx
@@ -0,0 +1,10 @@
+export default function ShiftsApp() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Shifts Management</h1>
+      <p>Your app is deployed. Replace this with your full UI.</p>
+    </div>
+  );
+}
+
+
